cbc52879e55378f14980f2dcb6d1e6084c4bb9db
GAGAN-141 <85050673+Gagan0141@users.noreply.github.com>
2025-09-10 11:59:28 +0530

    Delete routes directory

--- routes/patchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/userController');
-
-router.patch('/:id', controller.updateUser);
-
-module.exports = router;
